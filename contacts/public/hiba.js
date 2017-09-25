59a1d038f61abf0dd62ba33371928a0cde5e7d1c
hibaAkroush <hiba.akr@gmail.com>
2017-09-25 19:33:27 -0400

    server can send to the front end

--- contacts/public/hiba.js
+++ contacts/public/hiba.js
@@ -1,27 +1,36 @@
 var app = angular.module('myApp', []);
 
-app.controller("AppCtrl", function($scope) {
+app.controller("AppCtrl", function($scope,$http) {
 
-    person1 = {
-    	name: "hiba",
-    	contact: "baba",
-    	phone: "0548674641",
-    	condition: "none"
-    }
-        person2 = {
-    	name: "sameera",
-    	contact: "mama",
-    	phone: "1514541",
-    	condition: "none"
-    }
-        person3 = {
-    	name: "soso",
-    	contact: "babamama",
-    	phone:  "56481585858",
-    	condition: " nn"
-    }
-    var people = [person1,person2,person3]
-    $scope.people = people
-
+$http.get('/people').then(function(response){
+	console.log("i got data",response)
+	$scope.people = response.data
+})
+	   //  person1 = {
+    // 	name: "hiba",
+    // 	contact: "baba",
+    // 	phone: "0548674641",
+    // 	condition: "none"
+    // }
+    //     person2 = {
+    // 	name: "sameera",
+    // 	contact: "mama",
+    // 	phone: "1514541",
+    // 	condition: "none"
+    // }
+    //     person3 = {
+    // 	name: "soso",
+    // 	contact: "babamama",
+    // 	phone:  "56481585858",
+    // 	condition: " nn"
+    // }
+    // var people = [person1,person2,person3]	
+    // $scope.people = people
 });
+
+
+app.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);
+
 
--- contacts/server.js
+++ contacts/server.js
@@ -2,12 +2,39 @@
 var express = require('express');
 var app = express()
 var mongojs = require('mongojs')
-var db = mongojs('people')
+var db = mongojs('people',['people'])
 app.use(express.static(__dirname + '/public'))
 
 
-app.get('/',function (req, res) {
-  console.log('Hello World!')
+app.get('/people',function (req, res) {
+console.log("recived a get ")
+	    person1 = {
+    	name: "hiba",
+    	contact: "baba",
+    	phone: "0548674641",
+    	condition: "none"
+    }
+        person2 = {
+    	name: "sameera",
+    	contact: "mama",
+    	phone: "1514541",
+    	condition: "none"
+    }
+        person3 = {
+    	name: "soso",
+    	contact: "babamama",
+    	phone:  "56481585858",
+    	condition: " nn"
+    }
+    var people = [person1,person2,person3]	
+	res.json(people)   
+  // db.people.find(function(err,data){
+  // 	if(err){
+  // 		console.log("yl3an hah!")
+  // 	}
+
+  // })
+
 });
 
 app.listen(3000)
